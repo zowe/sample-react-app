00bc8ee34c755b8b8f116d90f3aa2f70f8f49cba
Suneeth Keerthy <skeerthy@rocketsoftware.com>
2018-09-05 12:34:14 -0400

    Added simple state changing features
    Signed-off-by: Suneeth Keerthy <skeerthy@rocketsoftware.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,83 @@ import * as React from 'react';
 import './App.css';
 import { MVDResources } from './mvd-resources';
 
-class App extends React.Component {
+class App extends React.Component<any, any> {
+  constructor(props){
+    super(props);   
+    this.state = {
+          selectedValue: 'Radish',
+          inputText: "Input Text Here",
+          radioOption: "option1",
+          selectedCheckBoxes: new Set(),
+          textArea: "Input larger text here!"
+        };
+  };
+  
+  handleDropDownChange(e) {
+    this.setState({selectedValue: e.target.value});
+  };
+
+  handleInputTextChange(e) {
+    this.setState({inputText: e.target.value});
+  };
+
+  handleRadioChange(e) {
+    this.setState({radioOption: e.target.value});
+  }
+
+  handleCheckBoxChange(e) {
+    if (this.state.selectedCheckBoxes.has(e.target.value)) {
+      this.state.selectedCheckBoxes.delete(e.target.value);
+    } else {
+      this.state.selectedCheckBoxes.add(e.target.value);
+    }
+  }
+
+  handleTextAreaChange(e) {
+    this.setState({textArea: e.target.value});
+  }
+
+  nextPage() {
+    console.log("Load next page!");
+  }
+
   public render(): JSX.Element {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Sample App</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code>, rebuild, and reload.
           <br/>
+          <select value={this.state.selectedValue} onChange={this.handleDropDownChange.bind(this)}>
+            <option value="Orange">Orange</option>
+            <option value="Radish">Radish</option>
+            <option value="Cherry">Cherry</option>
+          </select>
+          <br/>
+          <input type="text" value={this.state.inputText} onChange={this.handleInputTextChange.bind(this)}/>
+          <br/>
+          <form action="">
+            <input type="radio" name="radio" value="option1" checked={this.state.radioOption == "option1"} 
+              onChange={this.handleRadioChange.bind(this)}/> Option 1<br/>
+            <input type="radio" name="radio" value="option2" checked={this.state.radioOption == "option2"}
+              onChange={this.handleRadioChange.bind(this)}/> Option 2<br/>
+            <input type="radio" name="radio" value="option3" checked={this.state.radioOption == "option3"}
+              onChange={this.handleRadioChange.bind(this)}/> Option 3<br/>
+          </form>
+          <br/>
+            <input type="checkbox" name="check1" value="check1" 
+              onChange={this.handleCheckBoxChange.bind(this)}/> Check 1<br/>
+            <input type="checkbox" name="check2" value="check2"
+              onChange={this.handleCheckBoxChange.bind(this)}/> Check 2<br/>
+            <input type="checkbox" name="check3" value="check3"
+              onChange={this.handleCheckBoxChange.bind(this)}/> Check 3<br/>
+          <br/>
+          <textarea rows={4} cols={50} value={this.state.textArea} onChange={this.handleTextAreaChange.bind(this)}>
+          </textarea>
+          <br/>
+          <button onClick={this.nextPage}>Next Page</button>
           <br/>
           <MVDResources.Consumer>
             {resources => (

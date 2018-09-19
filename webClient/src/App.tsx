108993cb7676367d7dbcc6c0493c27ca5255e816
1000TurquoisePogs <sgrady@rocketsoftware.com>
2018-09-19 13:07:44 -0400

    App with just hello world router
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- webClient/src/App.tsx
+++ webClient/src/App.tsx
@@ -21,19 +21,8 @@ class App extends React.Component<any, any> {
       actionType: "Launch",
       appTarget: "PluginCreate",
       parameters: 
-      `{"type":"connect",
-        "connectionSettings":{
-          "host":"localhost",
-          "port":23,
-          "deviceType":5,
-          "alternateHeight":60,
-          "alternateWidth":132,
-          "oiaEnabled": true,
-          "security": {
-            "type":0
-          }
-        }}`,
-      appId: "com.rs.mvd.tn3270",
+      `TODO`,
+      appId: "TODO",
       status: "Status will appear here.",
       helloText: "",
       helloResponse: "",
@@ -99,58 +88,9 @@ class App extends React.Component<any, any> {
   }
 
   sendAppRequest() {
-    var requestText = this.state.parameters;
-    var parameters = null;
-    /*Parameters for Actions could be a number, string, or object. The actual event context of an Action that an App recieves will be an object with attributes filled in via these parameters*/
-    try {
-      if (requestText !== undefined && requestText.trim() !== "") {
-        parameters = JSON.parse(requestText);
-      }
-    } catch (e) {
-      //requestText was not JSON
-    }
-    
-    let appId = this.state.appId;  
-    if (appId) {
-      let message = '';
-      /* JS within an iframe can reference objects of the page it is embedded in via window.parent.
-         With ZLUX, there's a global called ZoweZLUX which holds useful tools. So, a site
-         Can determine what actions to take by knowing if it is or isnt embedded in ZLUX via IFrame.
-      */
-      /* PluginManager can be used to find what Plugins (Apps are a type of Plugin) are part of the current ZLUX instance.
-         Once you know that the App you want is present, you can execute Actions on it by using the Dispatcher.
-      */              
-      let dispatcher = ZoweZLUX.dispatcher;
-      let pluginManager = ZoweZLUX.pluginManager;
-      let plugin = pluginManager.getPlugin(appId);
-      if (plugin) {
-        let type;
-        type = dispatcher.constants.ActionType[this.state.actionType];
-        let mode;
-        mode = dispatcher.constants.ActionTargetMode[this.state.appTarget];
-        
-        if (type != undefined && mode != undefined) {
-          let actionTitle = 'Launch app from sample app';
-          let actionID = 'org.zowe.zlux.sample.launch';
-          let argumentFormatter = {data: {op:'deref',source:'event',path:['data']}};
-          /*Actions can be made ahead of time, stored and registered at startup, but for example purposes we are making one on-the-fly.
-            Actions are also typically associated with Recognizers, which execute an Action when a certain pattern is seen in the running App.
-          */
-          let action = dispatcher.makeAction(actionID, actionTitle, mode,type,appId,argumentFormatter);
-          let argumentData = {'data':(parameters ? parameters : requestText)};
-          this.log.info((message = 'App request succeeded'));        
-          this.setState({status: message});
-          /*Just because the Action is invoked does not mean the target App will accept it. We've made an Action on the fly,
-            So the data could be in any shape under the "data" attribute and it is up to the target App to take action or ignore this request*/
-          dispatcher.invokeAction(action,argumentData);
-        } else {
-          this.log.warn((message = 'Invalid target mode or action type specified'));        
-        }
-      } else {
-        this.log.warn((message = 'Could not find App with ID provided'));
-      }
-      this.setState({status: message});
-    }
+    let message = 'Unimplemented!';
+    this.log.warn(message);
+    this.setState({status: message});
   }
 
 
